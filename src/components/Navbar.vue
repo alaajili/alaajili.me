755265ca50d83c4cb8e0769a99c953fb17884ded
alaajili <abderrahmanelaajili0@gmail.com>
2024-05-27 19:48:30 +0100

    edit navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -23,19 +23,19 @@
           </li>
         </ul>
       </div>
-      <div class="md:hidden">
+      <div class="md:hidden text-neutral-400">
         <button @click="isMenuOpen = !isMenuOpen">
-          <svg v-if="!isMenuOpen" class="w-8 h-8 transition-transform duration-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <svg v-if="!isMenuOpen" class="hover:text-neutral-700 hover:dark:text-neutral-100 w-8 h-8 transition-transform duration-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
           </svg>
-          <svg v-else class="w-8 h-8 transition-transform duration-500 transform rotate-90" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <svg v-else class="hover:text-neutral-700 hover:dark:text-neutral-100 w-8 h-8 transition-transform duration-500 transform rotate-90" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
           </svg>
         </button>
         <transition name="fade">
           <ul v-if="isMenuOpen" class="flex flex-col text-xl hover:cursor-pointer text-neutral-400">
             <li class="hover:text-neutral-700 hover:dark:text-neutral-100">
-              <a>HOME</a>
+              <a @click="isMenuOpen = !isMenuOpen">HOME</a>
             </li>
             <li class="hover:text-neutral-700 hover:dark:text-neutral-100">
               <a>ABOUT</a>
